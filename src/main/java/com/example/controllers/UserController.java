3c29c650d50908789a20555640bec74759ccfc64
Evhen <malysh.evgeniy@gmail.com>
2023-08-22 10:03:13 +0300

    add openApi annotations

--- src/main/java/com/example/controllers/UserController.java
+++ src/main/java/com/example/controllers/UserController.java
@@ -3,9 +3,15 @@ package com.example.controllers;
 import com.example.domain.dto.UserRegistration;
 import com.example.domain.models.User;
 import com.example.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,23 +20,54 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final UserService userService;
 
+    @Operation(summary = "Find all users")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully retrieve page of users",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = User.class)))})
     @GetMapping("/all")
     Page<User> getAll(@RequestParam("size") int size, @RequestParam("page") int page) {
         var pageable = Pageable.ofSize(size).withPage(page);
         return userService.findAll(pageable);
     }
 
+    @Operation(summary = "Full text search by users firstname and lastname")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully retrieve collection of users",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = User.class)))})
     @GetMapping("/search/{q}")
     Iterable<User> search(@PathVariable("q") String query) {
         return userService.search(query);
     }
 
+    @Operation(summary = "Save new user")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Successfully save the user",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = User.class)))})
     @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
     User save(@RequestBody UserRegistration user) {
         return userService.save(user);
     }
 
+    @Operation(summary = "Delete existing user")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Successfully delete the user")})
     @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     void delete(@PathVariable("id") String id) {
         userService.deleteById(id);
     }

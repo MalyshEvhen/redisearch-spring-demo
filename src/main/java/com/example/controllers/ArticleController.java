ecdfafdac367bbdac178d5f5c2fb53b034188fae
Evhen <malysh.evgeniy@gmail.com>
2023-08-23 15:46:02 +0300

    add swagger annotations

--- src/main/java/com/example/controllers/ArticleController.java
+++ src/main/java/com/example/controllers/ArticleController.java
@@ -8,13 +8,16 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-
+@Validated
+@Tag(name = "Articles", description = "REST controller for managing museum articles.")
 @RestController
 @RequestMapping("/api/articles")
 @RequiredArgsConstructor
@@ -25,7 +28,8 @@ class ArticleController {
     @ApiResponses(value = {
             @ApiResponse(
                     responseCode = "200",
-                    description = "Successfully retrieved the list of articles",
+                    description = "Successfully retrieved the list of articles or empty list," +
+                            " if no articles is present in DB",
                     content = @Content(
                             mediaType = "application/json",
                             schema = @Schema(implementation = Article.class)))})
@@ -35,6 +39,20 @@ class ArticleController {
         return articleService.findAll(pageable);
     }
 
+    @Operation(summary = "Find article by ID")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully retrieved article",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Article.class))),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "ID is invalid"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Article not found")})
     @GetMapping("/get-by-id/{id}")
     Article byId(@PathVariable("id") String id) {
         return articleService.findById(id);
@@ -44,7 +62,8 @@ class ArticleController {
     @ApiResponses(value = {
             @ApiResponse(
                     responseCode = "200",
-                    description = "Successfully retrieved the list of articles",
+                    description = "Successfully retrieved the list of articles or empty list," +
+                            " if no articles been found by author ID",
                     content = @Content(
                             mediaType = "application/json",
                             schema = @Schema(implementation = Article.class)))})
@@ -63,7 +82,10 @@ class ArticleController {
                     description = "Article created successfully",
                     content = @Content(
                             mediaType = "application/json",
-                            schema = @Schema(implementation = Article.class)))})
+                            schema = @Schema(implementation = Article.class))),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Article request is not valid.")})
     @PostMapping("/save")
     @ResponseStatus(HttpStatus.CREATED)
     Article save(@RequestBody ArticlePostRequest article) {
